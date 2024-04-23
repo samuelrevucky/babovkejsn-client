bf23172789a319119fa78f95db6774bed8cfb61c
Samuel <revucky2@uniba.sk>
2024-04-23 20:12:22 +0200

    Debugging

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,15 @@ import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+    console.log("middleware");
     if (request.nextUrl.pathname.startsWith('/user')) {
         const token: string | undefined = request.cookies.get('authtoken')?.value;
         if (token === undefined) {
             return NextResponse.redirect(new URL('/login', request.url));
         }
         else {
-            //console.log("ahoj");
-            //console.log(jwt.decode(token));
+            console.log("ahoj");
+            console.log(jwt.decode(token));
         }
     }
     if (request.nextUrl.pathname.startsWith('/login')) {
