3973ec8193959d1557d71b065818f3d45ce360b0
Samuel <revucky2@uniba.sk>
2024-04-24 20:11:30 +0200

    Bug fix

--- app/(common)/user/get_token.ts
+++ app/(common)/user/get_token.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
-export function getToken() {
+export async function getToken() {
     "use server";
     const cookieStore = cookies();
     return cookieStore.get('authtoken')?.value;
