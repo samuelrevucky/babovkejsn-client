e5b8910abb17b6b144a07da61a786b7adb359af4
Samuel <revucky2@uniba.sk>
2024-04-24 18:07:48 +0200

    Bug fix

--- app/(common)/login/login.ts
+++ app/(common)/login/login.ts
@@ -1,12 +1,11 @@
 "use server";
 
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
-export async function Login(email: string, password: string, rememberMe: boolean) {
+export async function login(email: string, password: string, rememberMe: boolean) {
     "use server";
-    const router = useRouter();
     const cookieStore = cookies();
     await axios.post(process.env.SERVER + '/api/authenticate', {
         mail: email,
@@ -18,8 +17,7 @@ export async function Login(email: string, password: string, rememberMe: boolean
     .then(function (response) {
         if (response.data.authenticated) {
             cookieStore.set('authtoken', response.data.token, )
-            router.push("/user/profile");
-            router.refresh();
+            redirect("/user/profile");
         }
         else {
             throw Error;
